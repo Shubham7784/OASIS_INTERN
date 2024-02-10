18bf8342a94588e28af854fe0d41d28dacd818a9
Shubham Sharma <shubhamshubham7784@gmail.com>
2024-02-10 23:57:14 +0530

    PASSWORD_GENERATOR

--- PASSWORD_GENERATOR/Password_generator.py
+++ PASSWORD_GENERATOR/Password_generator.py
@@ -10,7 +10,7 @@ checkbutton3 = IntVar()
 specific_symbol = StringVar()
 pass_length = StringVar()
 c_password = ""
-
+password = ""
 def generate_pass():
     check_b1 = checkbutton1.get()
     check_b2 = checkbutton2.get()
@@ -19,11 +19,10 @@ def generate_pass():
     pass_alpha = ['a','A','b','B','c','C','d','D','e','E','f','F','g','G','h','H','i','I','j','J','k','K','l','L','m','M','n','N','o','O','p','P','q','Q','r','R','s','S','t','T','u','U','v','V','w','W','x','X','y','Y','z','Z']
     pass_num = ['0','1','2','3','4','5','6','7','8','9']
     pass_sym = ['@']
-    password = ""
+    global password
     global c_password    
     l_alpha = 0
     l_num = 0
-    # l = (l+1) if(l%2!=0) else l
     if(check_b1==1 and check_b2==1 and check_b3==1):
         l_alpha = int(l/2) 
         l_num = l-(l_alpha+len(pass_sym))
@@ -51,17 +50,17 @@ def generate_pass():
     if(check_b3==1):
         a = r.sample(pass_sym,len(pass_sym))
         password+= "".join(a)
-    
-    p = Label(window,text=password,font=('aerial',10,'bold'))
-    copy_button = Button(window,text="COPY",font=('aerial',10,'bold'),command=copy)
-    p.pack()
-    copy_button.pack()
     c_password = password
+    p = Label(window,text=password,font=('aerial',10,'bold'))
+    p.pack()
+    copy_button = Button(window,text="COPY",font=('aerial',10,'bold'),command=copy)
+    copy_button.pack()
     password = ""
 def copy():
     global c_password
     window.clipboard_clear()
     window.clipboard_append(c_password)
+
 msg = Label(window,text="What would you like include in your Password",font=('aerial',10,'bold'))
 option1 = Checkbutton(window,text="Alphabets",variable=checkbutton1)
 option2 = Checkbutton(window,text="Numbers",variable=checkbutton2)
@@ -69,7 +68,7 @@ option3 = Checkbutton(window,text="Symbols",variable=checkbutton3)
 len_label = Label(window,text="Password Length",font=('aerial',10,'bold'))
 len_val = Entry(window,textvariable=pass_length)
 gen_button = Button(window,text="GENERATE",command=generate_pass)
-sym_if = Label(window,text="Would you like to include any other symbol",font=('aerial',10,'bold'))
+sym_if = Label(window,text="Would you like to include any other symbol (only one )",font=('aerial',10,'bold'))
 sym_if_var = Entry(window,textvariable=specific_symbol)
 msg.pack()
 option1.pack()
@@ -80,4 +79,5 @@ len_val.pack()
 sym_if.pack()
 sym_if_var.pack()
 gen_button.pack()
+
 window.mainloop()
